563f8560e4be0914b5683f86f3cbb2c8059ae6a9
Viktor Þór <viktorthorr@gmail.com>
2020-04-04 14:43:32 +0000

    Challenges byrjunarvirkni án db

--- app/src/main/java/com/company/gaia/Fragments/ChallengesFragment.java
+++ app/src/main/java/com/company/gaia/Fragments/ChallengesFragment.java
@@ -1,13 +1,18 @@
 package com.company.gaia.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.company.gaia.Activities.challenge_activity;
 import com.company.gaia.R;
 
 public class ChallengesFragment extends Fragment {
@@ -15,6 +20,30 @@ public class ChallengesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_challenges, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_challenges, container, false);
+
+        String[] challengesItems = { "Vera Ómar í einn dag", "Prumpa minna", "Vera vegan í einn dag", "Hættu að vera sóði",
+        "Nota fjölnotapoka"};
+
+        ListView listView = view.findViewById(R.id.challengeList);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                challengesItems
+        );
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), challenge_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
