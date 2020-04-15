6f13095804e3e01671bedbb5ce39e5633265c1c6
Viktor Þór <viktorthorr@gmail.com>
2020-04-15 20:40:16 +0000

    Search bull

--- app/src/main/java/com/company/gaia/Fragments/SearchFragments.java
+++ app/src/main/java/com/company/gaia/Fragments/SearchFragments.java
@@ -3,6 +3,8 @@ package com.company.gaia.Fragments;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,7 @@ import com.company.gaia.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -33,7 +36,9 @@ public class SearchFragments extends Fragment {
     private ListView lstSearch;
     private EditText txtSearch;
     public ArrayList<String> userList = new ArrayList<String>();
+    public ArrayList<Double> scoreList = new ArrayList<Double>();
     public GaiaAPI gaiaAPI;
+    private ArrayAdapter<String> userAdapter;
 
     @Nullable
     @Override
@@ -58,28 +63,54 @@ public class SearchFragments extends Fragment {
                 }
 
                 List<User> users = response.body();
-                System.out.println(users);
 
                 for (User user : users) {
                     userList.add(user.getuname());
+                    scoreList.add(user.getCurrIndex());
                 }
 
-                ArrayAdapter<String> userAdapter =
-                        new ArrayAdapter<String>(getActivity(), R.layout.custom_simple_list_item_2, R.id.text1, userList){
-                            @Override
-                            public View getView(int position, View convertView, ViewGroup parent) {
-                                View view = super.getView(position, convertView, parent);
-                                TextView text1 = view.findViewById(R.id.text1);
-                                TextView text2 = view.findViewById(R.id.text2);
+                userAdapter = new ArrayAdapter<String>(getActivity(), R.layout.custom_simple_list_item_2, R.id.text1, userList){
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
 
+                        User currUser = users.get(position);
+                        TextView text1 = view.findViewById(R.id.text1);
+
+                        TextView text2 = view.findViewById(R.id.text2);
+                        Double d = currUser.getOriginalIndex();
+                        String str = d + "";
+
+                        String search = txtSearch.getText().toString();
+                        for(int i = 0; i < userList.size(); i++) {
+                            if(search == userList.get(i)) {
                                 text1.setText(users.get(position).getuname());
-                                double d = users.get(position).getCurrIndex();
-                                String str = d+"";
-                                text2.setText(str);
-                                return view;
+                                text2.setText(scoreList.get(position).toString());
                             }
-                        };
+                        }
+                        return view;
+                    }
+                };
+
                 lstSearch.setAdapter(userAdapter);
+
+                txtSearch.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        userAdapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+
             }
 
             @Override
@@ -89,3 +120,28 @@ public class SearchFragments extends Fragment {
         });
     }
 }
+/**
+ userAdapter = new ArrayAdapter<User>(getActivity(), R.layout.custom_simple_list_item_2, R.id.text1, users){
+@Override
+public View getView(int position, View convertView, ViewGroup parent) {
+View view = super.getView(position, convertView, parent);
+
+TextView text1 = view.findViewById(R.id.text1);
+TextView text2 = view.findViewById(R.id.text2);
+
+String search = txtSearch.getText().toString();
+for(int i = 0; i < userList.size(); i++) {
+if(search == userList.get(i)) {
+text1.setText(users.get(position).getuname());
+double d = users.get(position).getCurrIndex();
+String str = d + "";
+text2.setText(str);
+
+
+}
+}
+
+return view;
+}
+};
+ **/
