5389b8255f1e3972a4aef09e4401c774e5e02a2b
Viktor Þór <viktorthorr@gmail.com>
2020-04-16 09:55:31 +0000

    Search complete

--- app/src/main/java/com/company/gaia/Fragments/HomeFragment.java
+++ app/src/main/java/com/company/gaia/Fragments/HomeFragment.java
@@ -31,7 +31,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
-/*
+
     public GaiaAPI gaiaAPI;
     private ListView usernameTxt;
     public ArrayAdapter<String> userAdapter;
@@ -45,15 +45,11 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         gaiaAPI = APIclient.getGaiaClient().create(GaiaAPI.class);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        usernameTxt = view.findViewById(R.id.usernameTxt);
         getLoggedIn();
-
-
-        }
 
         return view;
 
-    }
+        }
 
     private void getLoggedIn() {
         Call<User> call = gaiaAPI.getLoggedIn();
@@ -68,76 +64,15 @@ public class HomeFragment extends Fragment {
 
                 User user = response.body();
 
-                user.getName();
-                user.getEmail();
-                System.out.println("*_______________*");
-                System.out.println(user.getName());
-                System.out.println(user.getEmail());
-                System.out.println("*------------------*");
+
             }
 
-            ArrayAdapter<String> userAdapter =
-                new ArrayAdapter<String>(getActivity(),R.id.custom_simple_list_item_2, R.id.text1) {
-                @Override
-                public View getView(View convertView ViewGroup parent) {
-                View view = super.getView(convertView, parent);
-                TextView text1 = view.findViewById(R.id.text1);
-
-                text1.setText(user.);
-
-                }
-            }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
                 System.out.println(t.getMessage());
             }
-        });*/
-    }
-/*
-    private void getUsername() {
-        System.out.println("ert inní getUsername");
-        Call<List<User>> call = gaiaAPI.getUsers();
-        System.out.println("Herna er callið: " + call);
-
-        call.enqueue(new Callback<List<User>>() {
-            @Override
-            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-                System.out.println("inside onResponse in HomeFragment");
-
-                if (!response.isSuccessful()) {
-                    System.out.println("no response found my guy");
-                }
-
-                List<User> users = response.body();
-
-                for (User user : users) {
-                    System.out.println(user.getuname());
-                    userList.add(user.getuname());
-                }
-
-                ArrayAdapter<String> userAdapter =
-                        new ArrayAdapter<String>(getActivity(), R.layout.activity_user, R.id.textViewUser, userList){
-                            @Override
-                            public View getView(int position, View convertView, ViewGroup parent) {
-                                View view = super.getView(position, convertView, parent);
-                                TextView textViewUser = view.findViewById(R.id.textViewUser);
-
-                                textViewUser.setText(users.get(position).getName());
-                                return view;
-                            }
-                        };
-
-               textViewUser.setAdapter(userAdapter);
-
-            }
-
-            @Override
-            public void onFailure(Call<List<User>> call, Throwable t) {
-                System.out.println(t.getMessage());
-            }
         });
     }
 }
- */
 
